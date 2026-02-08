ab1534a853def14155068c273395b148938b3c76
Nabil <nabilaltamash@gmail.com>
2026-02-09 01:50:11 +0600

    chore: remove unit of work abstraction

--- backend/dotnet/src/Core/Workforce.AppCore/Abstractions/IUnitOfWork.cs
+++
@@ -1,8 +0,0 @@
-namespace Workforce.AppCore.Abstractions;
-
-public interface IUnitOfWork
-{
-    Task BeginAsync(CancellationToken cancellationToken = default);
-    Task CommitAsync(CancellationToken cancellationToken = default);
-    Task RollbackAsync(CancellationToken cancellationToken = default);
-}
