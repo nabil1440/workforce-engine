18aa0f3b6ba838a2ee95ed886cb621f0fb8d441e
Nabil <nabilaltamash@gmail.com>
2026-02-09 16:05:38 +0600

    feat(api): add swagger ui

--- backend/dotnet/src/Hosts/Workforce.Api/Program.cs
+++ backend/dotnet/src/Hosts/Workforce.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 using Workforce.Api.Startup;
 using Workforce.AppCore.Services;
 using Workforce.AppCore.Services.Implementations;
@@ -7,8 +9,23 @@ using Workforce.Infrastructure.Sql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Workforce API",
+        Version = "v1"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath))
+    {
+        options.IncludeXmlComments(xmlPath);
+    }
+});
 
 builder.Services.AddSqlInfrastructure(builder.Configuration);
 builder.Services.AddMongoInfrastructure(builder.Configuration);
@@ -24,10 +41,12 @@ builder.Services.AddScoped<IDashboardService, DashboardService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.MapOpenApi();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Workforce API v1");
+    options.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 app.MapControllers();
