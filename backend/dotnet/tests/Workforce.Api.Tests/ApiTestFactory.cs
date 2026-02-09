1f757b0ada39ceb277c100630c6f015514b8b791
Nabil <nabilaltamash@gmail.com>
2026-02-09 13:13:43 +0600

    test: add api integration tests

---
+++ backend/dotnet/tests/Workforce.Api.Tests/ApiTestFactory.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Workforce.AppCore.Services;
+
+namespace Workforce.Api.Tests;
+
+public sealed class ApiTestFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(config =>
+        {
+            var values = new Dictionary<string, string?>
+            {
+                ["ApplyMigrationsOnStartup"] = "false"
+            };
+
+            config.AddInMemoryCollection(values);
+        });
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<IEmployeeService>();
+            services.AddSingleton<IEmployeeService, FakeEmployeeService>();
+        });
+    }
+}
